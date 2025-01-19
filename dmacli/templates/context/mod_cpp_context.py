0a430fae51ad26a7e7a4feb18fde728a96fa5b3f
Saitama- <wishiweresaitama@gmail.com>
2025-01-19 17:45:00 +0200

    Fix formatting in MOD_CPP_CONTEXT by adding semicolons and ensuring consistent use of quotes

--- dmacli/templates/context/mod_cpp_context.py
+++ dmacli/templates/context/mod_cpp_context.py
@@ -2,16 +2,16 @@ MOD_CPP_CONTEXT = {
     'name': 'mod.cpp',
     'context':
 """\
-name = "{{MODIFICATION_NAME_PLACEHOLDER}}"
-picture = ""
-logo = ""
-logoSmall = ""
-logoOver = ""
-tooltip = "{{MODIFICATION_NAME_PLACEHOLDER}}"
-overview = ""
-action = ""
-author = ""
-authorID = ""
+name = "{{MODIFICATION_NAME_PLACEHOLDER}}";
+picture = "";
+logo = "";
+logoSmall = "";
+logoOver = "";
+tooltip = "{{MODIFICATION_NAME_PLACEHOLDER}}";
+overview = "";
+action = "";
+author = "";
+authorID = "";
 version = "";
 """
 }
